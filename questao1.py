df8b06713e70c93c044434b1eb6282d059160036
anittagabriela82 <anittagabriela82@gmail.com>
2025-08-22 19:06:30 -0300

    ajustado

--- questao1.py
+++ questao1.py
@@ -6,6 +6,7 @@ print("[1] - Hambúrguer - R$25,00")
 print("[2] - Batata Frita - R$15,00")   
 print("[3] - Refrigerante - R$8,00")
 resp = int(input("Escolha o número do produto desejado: "  ))
+computador = random.randint(1, 3)
 if resp == 1:
     print ("Você escolheu Hambúrguer - R$25,00")        
 elif resp == 2:
@@ -13,4 +14,5 @@ elif resp == 2:
 elif resp == 3:
         print (" Você escolheu Refrigerante - R$8,00")
 else:
-        print (" Opção inválida ")          
+        print (" Opção inválida ")
+                  
--- questao3.py
+++ questao3.py
@@ -1,9 +1,11 @@
+import random
 print ("------vamos jogar pedra, papel ou tesoura?------")
 print (" escolha a opçao a baixo:")
 print("[1] pedra ")
 print("[2] papel")   
 print("[3] tesoura")
 resp = int(input("sua escolha: "))
+computador = random.randint(1, 3)
 if resp == 1:
     print ("Você escolheu pedra")        
 elif resp == 2:
@@ -12,3 +14,19 @@ elif resp == 3:
         print ("Você escolheu tesoura")
 else:
         print ("escolha invalida")
+opcoes = {1: "Pedra", 2: "Papel", 3: "Tesoura"}
+if resp not in [1, 2, 3]:
+    print("Escolha inválida")
+else:
+    print(f"\nVocê escolheu: {opcoes[resp]}")
+    print(f"Computador escolheu: {opcoes[computador]}\n")
+
+    # Verifica quem venceu
+    if resp == computador:
+        print("Empate!")
+    elif (resp == 1 and computador == 3) or \
+         (resp == 2 and computador == 1) or \
+         (resp == 3 and computador == 2):
+        print("Você venceu!")
+    else:
+        print("Computador venceu!")
